7b2e6b5670e808698df6849b225164f2f60d884c
katiemcpeak <katiemcpeak@Katies-MacBook-Pro.local>
2015-04-15 12:18:15 -0400

    made on click for starfish rotation, trying to fix blowfish

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var $btnExpand = $('.btn-expand');
 var $panel = $('.panel');
+var $spinStar = $('.spinstar');
 
 $btnExpand.on('click', function () {
     $panel.toggleClass('js-panel-expand'); 
@@ -15,15 +16,19 @@ var $btnRotateLeft = $('.btn-rotate2');
 var $blowFishGreen = $('.blowfishgreen');
 
 $btnShowHide.on('click', function () {
-    $blowFish.toggleClass('js-show-hide'); 
+    $blowFish.toggleClass('blowfish'); 
 });
 
 $btnRotate.on('click', function () {
-    $blowFishBlue.toggleClass('js-rotate'); 
+    $blowFishBlue.toggleClass('blowfishblue'); 
 });
 
 $btnRotateLeft.on('click', function () {
-    $blowFishGreen.toggleClass('js-rotate-left'); 
+    $blowFishGreen.toggleClass('blowfishgreen'); 
+});
+
+$spinStar.on('click', function () {
+    $spinStar.toggleClass('js-active');
 });
 
 $('.scrolldown').localScroll();
