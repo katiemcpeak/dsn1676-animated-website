7b92ad751deca2769cfa3ff8c2c44cee93d86817
katiemcpeak <katiemcpeak@Katies-MacBook-Pro.local>
2015-04-15 13:53:09 -0400

    undo a bunch of things

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 var $btnExpand = $('.btn-expand');
 var $panel = $('.panel');
 var $spinStar = $('.spinstar');
-var $blowFish = $('.blowfish');
-var $blowFishBlue = $('.blowfishblue');
-var $blowFishGreen = $('.blowfishgreen');
 
 $btnExpand.on('click', function () {
     $panel.toggleClass('js-panel-expand'); 
 });
+
+var $btnShowHide = $('.btn-show-hide');
+var $blowFish = $('.blowfish');
+
+var $btnRotate = $('.btn-rotate');
+var $blowFishBlue = $('.blowfishblue');
+
+var $btnRotateLeft = $('.btn-rotate2');
+var $blowFishGreen = $('.blowfishgreen');
 
 $btnShowHide.on('click', function () {
     $blowFish.toggleClass('js-show-hide'); 
